b414f07f672883c7b4427ceb4c9b584d15d6e0db
Du <dubydu97@gmail.com>
2018-04-25 22:46:43 +0700

    finished challenges in Fuctions Chapter

--- challenges/Chapter 6/MyPlayground.playground/Contents.swift
+++ challenges/Chapter 6/MyPlayground.playground/Contents.swift
@@ -36,7 +36,7 @@ for index2 in stride(from: 10, through: 22, by: 4){
 }
 print("\n")
 
-for index3 in stride(from: 10.0, to: 9.0, by: -0.1) {
+for index3 in stride(from: 10.0, through: 9.0, by: -0.1) {
     print(index3)
 }
 
@@ -44,17 +44,43 @@ for index3 in stride(from: 10.0, to: 9.0, by: -0.1) {
 
 // challegen B: It's prime time
 func isNumberDivisible (_ number: Int, by division: Int) -> Bool{
-    if (number % division == 0) {
+    if number % division == 0 {
         return true
     }else {
         return false
     }
 }
 
-//func isPrime(_ number: Int) -> Bool {
-//
-//}
+func isPrime(_ number: Int) -> Bool {
+    
+    if number <= 1 {
+        return false
+    }
+    for division in 2..<number {
+        if isNumberDivisible(number, by: division){
+            return false
+        }
+    }
+    return true
+}
+isPrime(13)
 
+
+
+
+// challenges C: Recursive
+func fibonacci(_ number: Int) -> Int {
+    if number < 0 {
+        return 0
+    }else if number == 2 || number == 1 {
+        return 1
+    }else {
+        return fibonacci(number - 1) + fibonacci(number - 2)
+    }
+}
+fibonacci(10)
+fibonacci(5)
+fibonacci(2)
 
 
 
